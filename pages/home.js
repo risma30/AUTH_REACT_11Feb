951488df9893e96729f839a54f12653982040ce2
risma30 <78523932+risma30@users.noreply.github.com>
2021-03-30 20:23:19 +0700

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,75 +1,129 @@
-import React from 'react'
-import axios from 'axios'
-import Navbarr from '../components/navbar'
-import { Navbar } from 'react-bootstrap'
+import React from 'react'
+import axios from 'axios'
+import NavBar from '../components/navbar'
 
-class Home extends React.Component{
+class Home extends React.Component{
     constructor(){
         super()
-        this.state = {
-            token: "",
-            userName: null,
-            pegawaiCount: 0
+        this.state = {
+            token: "",
+            userName: null,
+            pegawaiCount: 0,
+            siswaCount:0,
+            pelanggaranCount:0
         }
-        if (localStorage.getItem("token")) {
-            this.state.token = localStorage.getItem("token")
-        }else {
-            window.location = "/login"
+        if (localStorage.getItem("token")) {
+            this.state.token = localStorage.getItem("token")
+        } else {
+            window.location = "/login"
         }
     }
-    
-    headerConfig = () => {
-        let header = {
-            headers: { Authorization: `Bearer ${this.state.token}` }
+    headerConfig = () => {
+        let header = {
+            headers: { Authorization: `Bearer ${this.state.token}` }
         }
-        return header
+        return header
     }
-
-    getUser = () => {
-        let user = JSON.parse(localStorage.getItem('user'))
-        this.setState({userName: user[0].username})
+    getUser = () => {
+        let user = JSON.parse(localStorage.getItem('user'))
+        this.setState({userName: user[0].username})
     }
-
     getPegawai = () => {
         let url = "http://localhost:2000/pegawai";
+        // mengakses api untuk mengambil data pegawai
         axios.get(url, this.headerConfig())
         .then(response => {
-            this.setState({pegawaiCount: response.data.count});
+          // mengisikan data dari respon API ke array pegawai
+          this.setState({pegawaiCount: response.data.count});
         })
-
-        .catch(error => {
-            console.log(error);
+        .catch(error => {
+          console.log(error);
         });
-
     }
-    
+    getSiswa = () => {
+        let url = "http://localhost:2000/siswa";
+        // mengakses api untuk mengambil data siswa
+        axios.get(url, this.headerConfig())
+        .then(response => {
+          // mengisikan data dari respon API ke array siswa
+          this.setState({siswaCount: response.data.count});
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    getPelanggaran = () => {
+        let url = "http://localhost:2000/pelanggaran";
+        // mengakses api untuk mengambil data pelanggaran
+        axios.get(url, this.headerConfig())
+        .then(response => {
+          // mengisikan data dari respon API ke array pelanggaran
+          this.setState({pelanggaranCount: response.data.count});
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    componentDidMount(){
+        this.getUser()
+        this.getPegawai()
+        this.getSiswa()
+        this.getPelanggaran()
+    }
+
     render(){
         return(
-        <div>
-            <Navbar/>
-            <div className="container mt-2">
-                <h3 className="my-2">
-                    <strong>Welcome back, {this.state.userName}</strong>
-                </h3>
-                <div className="row">
-                    {/* pegawai count */}
-                    <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
-                        <div className="card">
-                            <div className="card-body bg-success">
-                                <h4 className="text-dark">
-                                    <strong>Jumlah Pegawai</strong>
-                                </h4>
-                                <h1 className="text-white">
-                                    <strong>{this.state.pegawaiCount}</strong>
-                                </h1>
+            <div>
+                <NavBar />
+                <div className="container mt-2">
+                    <h3 className="my-2">
+                        <strong>Welcome back, {this.state.userName}</strong>
+                    </h3>
+                    <div className="row">
+                        {/* pegawai count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-info">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Pegawai</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.pegawaiCount}</strong>
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        {/* siswa count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-success">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Siswa</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.siswaCount}</strong>
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        {/* pelanggaran count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-warning">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Pelanggaran</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.pelanggaranCount}</strong>
+                                    </h1>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
-    }
-}
+    }}
 
-export default Home
+
+export default Home
