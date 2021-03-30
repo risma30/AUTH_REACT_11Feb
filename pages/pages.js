469d6e654335705211b7e522437367a8eeaad793
risma30 <78523932+risma30@users.noreply.github.com>
2021-03-30 20:23:57 +0700

    Update pages.js

--- pages/pages.js
+++ pages/pages.js
@@ -4,18 +4,22 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './home'
 import Login from './login'
 import Register from './register'
+import Jurusan from './jurusan'
 import Pegawai from './pegawai'
 import Siswa from './siswa'
 import Pelanggaran from './pelanggaran'
+import PelanggaranSiswa from './pelanggaran_siswa'
 
 const Pages = () => (
     <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
+        <Route path='/jurusan' component={Jurusan}/>
         <Route path='/pegawai' component={Pegawai}/>
         <Route path='/siswa' component={Siswa}/>
         <Route path='/pelanggaran' component={Pelanggaran}/>
+        <Route path='/pelanggaran_siswa' component={PelanggaranSiswa}/>
     </Switch>
 )
 
